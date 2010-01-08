16397482880774b0d9b26700a7f7330631a48164
Vincent QUEMENER <vincent.quemener@c-s.fr>
2010-01-08 11:13:25 +0000

    Dernier changement de format de date pour le formulaire de recherche.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@1593 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- vigiboard/controllers/vigiboardrequest.py
+++ vigiboard/controllers/vigiboardrequest.py
@@ -447,7 +447,7 @@ class VigiboardRequest():
         # Dialogue de recherche
         tmpl_context.search_form = SearchForm('search_form', lang=self.lang,
                                               # TRANSLATORS : Format de date  
-                                              date_format=_('%Y-%m-%d %I:%M:%S %p'))
+                                              date_format=_('%Y-%m-%d %I:%M:%S %P'))
         
         # Dialogue de détail d'un événement
 
