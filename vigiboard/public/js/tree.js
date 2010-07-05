7b885e2211d160857f1e3fe438a874059575c07e
Francois POIROTTE <francois.poirotte@c-s.fr>
2010-07-05 16:44:21 +0000

    Fichier manquant lors du précédent commit.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@3601 b22e2e97-25c9-44ff-b637-2e5ceca36478

---
+++ vigiboard/public/js/tree.js
@@ -0,0 +1,75 @@
+/**
+ * VigiBoard, composant de Vigilo.
+ * (c) CSSI 2009-2010 <contact@projet-vigilo.org>
+ * Licence : GNU GPL v2 ou superieure
+ * 
+ */
+
+/*
+ * Affichage en arbre des groupes.
+ */
+var TreeGroup = new Class({
+    Implements: [Options, Events],
+
+    initialize: function(options) {
+        this.setOptions(options);
+
+        /* L'objet tree se réfère à un élément div*/
+        var container = new Element('div')
+        this.tree = new Jx.Tree({parent: container});
+
+        this.dlg = new Jx.Dialog({
+            label: this.options.title,
+            modal: true,
+            content: container,
+        });
+
+        var req = new Request.JSON({
+            method: "get",
+            url: this.options.url,
+            onSuccess: function(groups) {
+                groups = new Hash(groups.groups);
+                $each(groups, function(item, key) {
+                    this.addItem(key, item, this.tree);
+                }, this);
+            }.bind(this)
+        });
+        req.send();
+    },
+
+    /* Ajout d'un element à l'arbre */
+    addItem: function(idgroup, data, parent) {
+        var subfolder;
+        var children = new Hash(data.children);
+        if (children.getLength()) {
+            subfolder = new Jx.TreeFolder({
+                label: data.name,
+                data: idgroup,
+                image: this.options.app_path+"images/map-list.png",
+            });
+        }
+        else {
+            subfolder = new Jx.TreeItem({
+                label: data.name,
+                data: idgroup,
+                image: this.options.app_path+"images/map.png",
+            });
+        }
+
+        subfolder.addEvent("click", function() {
+            this.fireEvent('select', [subfolder]);
+            this.dlg.close();
+            return false;
+        }.bind(this));
+        parent.append(subfolder);
+
+        $each(children, function(item, key) {
+            this.addItem(key, item, subfolder);
+        }, this);
+    },
+
+    selectGroup: function() {
+        this.dlg.open();
+    }
+});
+
