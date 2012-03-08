0075a6815494e8b184c51e96c0a9fbf37ca39f37
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-03-08 15:00:23 +0100

    Conversion ODT -> RST pour doc util. de VigiBoard.
    Change-Id: Id981e023a0707e4a5cafc1ac77492d581740a749
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/549
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- doc/conf.py
+++ doc/conf.py
@@ -6,11 +6,14 @@ project = u'VigiBoard'
 
 pdf_documents = [
         ('admin', "admin-%s" % name, "%s : Guide d'administration" % project, u'Vigilo'),
+        ('util', "util-%s" % name, "%s : Guide d'utilisation" % project, u'Vigilo'),
 ]
 
 latex_documents = [
         ('admin', 'admin-%s.tex' % name, u"%s : Guide d'administration" % project,
          'AA100004-2/ADM00005', 'vigilo'),
+        ('util', 'util-%s.tex' % name, u"%s : Guide d'utilisation" % project,
+         'AA100004-2/UTI00002', 'vigilo'),
 ]
 
 execfile("../buildenv/doc/conf.py")
