ab06ef787aa40f25d9f66072ab2039f418949b22
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-11-09 11:17:40 +0100

    Bump des versions pour sortie de Vigilo v3.0.
    Change-Id: Ic408886aecd69097c715064854e1e0c420a71080
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/962
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ else:
 
 setup(
     name='vigilo-vigiboard',
-    version='2.1.2',
+    version='3.0.0',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     zip_safe=False, # pour pouvoir déplacer app_cfg.py
