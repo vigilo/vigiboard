d06e792338a02307e4a6896b6c19c8340643fbc1
Francois POIROTTE <francois.poirotte@c-s.fr>
2014-05-12 14:30:48 +0200

    Bump des versions
    Change-Id: I16667f5212c272c4185f14412f1fa67d6831f04c
    BuildBot: no-build
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1412
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>
    Tested-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ else:
 
 setup(
     name='vigilo-vigiboard',
-    version='3.4',
+    version='3.5',
     author='Vigilo Team',
     author_email='contact@projet-vigilo.org',
     zip_safe=False, # pour pouvoir déplacer app_cfg.py
