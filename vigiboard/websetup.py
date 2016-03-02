2bcebf54aa1c80d39c95ff76e5023a0602de4458
Francois POIROTTE <francois.poirotte@c-s.fr>
2016-03-02 10:31:13 +0100

    Mise à jour des copyrights
    Change-Id: I7f58dc9aeff1655d44cedae4981fc3dd75ead9dc
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1967
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- vigiboard/websetup.py
+++ vigiboard/websetup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ################################################################################
 #
-# Copyright (C) 2007-2015 CS-SI
+# Copyright (C) 2007-2016 CS-SI
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
