55f2d54b07065f9b2afd2f6f7b06a315610cd8a8
Francois POIROTTE <francois.poirotte@c-s.fr>
2017-08-24 10:16:20 +0200

    Bump version 4.1.0rc2
    Change-Id: Ifb90013a93116ec21e686a067104bf26d90517d9
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 setup(
     name='vigilo-vigiboard',
-    version='4.1.0rc1',
+    version='4.1.0rc2',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     zip_safe=False, # pour pouvoir déplacer app_cfg.py
