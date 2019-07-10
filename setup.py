5ae008a7c6204986136cc04fd41c56306f3ae1a9
Francois POIROTTE <francois.poirotte@c-s.fr>
2019-07-10 18:37:13 +0200

    Bump version v5.1.0alpha2
    Change-Id: I2509aca7890d11926f9919b9594e8bb475da6a4d
    BuildBot: no-build

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 setup(
     name='vigilo-vigiboard',
-    version='5.1.0a1',
+    version='5.1.0a2',
     author='Vigilo Team',
     author_email='contact.vigilo@c-s.fr',
     zip_safe=False, # pour pouvoir déplacer app_cfg.py
